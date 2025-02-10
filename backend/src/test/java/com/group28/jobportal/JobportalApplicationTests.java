a5072441c8e1c80ef8e549d469d484dc6a297215
Kerem00 <kerembicen94@gmail.com>
2025-02-10 17:12:15 +0300

    Backend template

---
+++ backend/src/test/java/com/group28/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group28.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
