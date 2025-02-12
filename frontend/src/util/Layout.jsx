8d73838e926e30095877ca3b61252255d00d3383
eko20 <mekinay@gmail.com>
2025-02-12 15:48:48 +0300

    volkan edit

--- frontend/src/util/Layout.jsx
+++ frontend/src/util/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import { Outlet } from "react-router";
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
