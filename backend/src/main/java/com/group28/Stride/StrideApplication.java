eca487e1831c39e51cda712917a068fc8142693b
Kerem <kerembicen94@gmail.com>
2025-02-17 10:02:03 +0300

    Changed template to Maven and added Spring Security

---
+++ backend/src/main/java/com/group28/Stride/StrideApplication.java
@@ -0,0 +1,13 @@
+package com.group28.Stride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrideApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/group28/Stride/StrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group28.Stride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
