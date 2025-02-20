8052f3dcddb9b94fe5fcf3d57b3a9d356de9de41
Kerem00 <kerembicen94@gmail.com>
2025-02-20 22:08:35 +0300

    Get user info

--- backend/src/main/java/com/group28/Stride/StrideApplication.java
+++ backend/src/main/java/com/group28/Stride/StrideApplication.java
@@ -3,27 +3,29 @@ package com.group28.Stride;
 import com.clerk.backend_api.helpers.jwks.AuthenticateRequest;
 import com.clerk.backend_api.helpers.jwks.AuthenticateRequestOptions;
 import com.clerk.backend_api.helpers.jwks.RequestState;
-import com.clerk.backend_api.models.errors.ClerkErrors;
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @SpringBootApplication
 @RestController
 public class StrideApplication {
 
-	public static void main(String[] args) throws ClerkErrors, Exception {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(StrideApplication.class, args);
 	}
 
 	@GetMapping("/hello")
-	public String hello(HttpServletRequest request) throws IOException {
+	public String hello(HttpServletRequest request) throws Exception {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("backend")
+				.load();
+
 		RequestState requestState = AuthenticateRequest.authenticateRequest(HttpConverter.convert(request), AuthenticateRequestOptions
-				.secretKey("sk_test_5MtSsaFSR688og6juJjo63GPAbnrWBTjApE4OmDvER")
+				.secretKey(dotenv.get("SECRET"))
 				.build());
 
 		if (requestState.claims().isPresent()) {
@@ -31,7 +33,19 @@ public class StrideApplication {
 		} else {
 			System.out.println("No claims");
 		}
-		
+
 		return requestState.isSignedIn() ? "true" : "false";
+
+//		Clerk sdk = Clerk.builder()
+//				.bearerAuth(dotenv.get("SECRET"))
+//				.build();
+//
+//		GetUserResponse res = sdk.users().get()
+//				.userId("user_2tDY5Zt8xMiIVvK4mvBqe7q0AIJ")
+//				.call();
+//
+//		if (res.user().isPresent()) {
+//			System.out.println(res.user().get().firstName());
+//		}
 	}
 }
