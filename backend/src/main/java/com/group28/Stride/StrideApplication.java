d421ed1cf6c0b40cca330f9dbc0fb15f82640107
Kerem00 <kerembicen94@gmail.com>
2025-03-19 02:05:14 +0300

    We need CORS

--- backend/src/main/java/com/group28/Stride/StrideApplication.java
+++ backend/src/main/java/com/group28/Stride/StrideApplication.java
@@ -6,6 +6,7 @@ import com.group28.Stride.util.GetUserInfo;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
@@ -17,6 +18,7 @@ public class StrideApplication {
 		SpringApplication.run(StrideApplication.class, args);
 	}
 
+	@CrossOrigin
 	@GetMapping("/hello")
 	public String hello(HttpServletRequest request) throws Exception {
 		Database.basicQuery("SELECT * FROM jobs");
