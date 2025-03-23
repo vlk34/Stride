fa8d3a005cbf1240a965c2cb086e7fde6dfc45f8
Kerem00 <kerembicen94@gmail.com>
2025-03-23 17:49:07 +0300

    Query for jobs

--- backend/src/main/java/com/group28/Stride/controller/UserController.java
+++ backend/src/main/java/com/group28/Stride/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.group28.Stride.controller;
 
+import com.group28.Stride.util.Database;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,7 +13,12 @@ import java.util.Map;
 public class UserController {
     @CrossOrigin
     @GetMapping("/search")
-    public List<Map<String, Object>> search() {
-
+    public List<Map<String, Object>> search(
+            @RequestParam String q,
+            @RequestParam String workstyle,
+            @RequestParam String jobtype,
+            @RequestParam String industry
+    ) {
+        return Database.jobQuery(q, workstyle, jobtype, industry);
     }
 }
--- backend/src/main/java/com/group28/Stride/util/Database.java
+++ backend/src/main/java/com/group28/Stride/util/Database.java
@@ -2,6 +2,10 @@ package com.group28.Stride.util;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Database {
     private static final Dotenv dotenv;
@@ -28,7 +32,30 @@ public class Database {
         }
     }
 
-    public static void jobQuery(String q, String workstyle, String jobtype, String industry) {
-
+    public static List<Map<String, Object>> jobQuery(String q, String workstyle, String jobtype, String industry) {
+        try {
+            Connection connection = DriverManager.getConnection(String.format("jdbc:postgresql://%s:%s/%s", dotenv.get("DB_HOST"), dotenv.get("DB_PORT"), dotenv.get("DB_NAME")), dotenv.get("DB_USER"), dotenv.get("DB_PASSWORD"));
+            PreparedStatement statement = connection.prepareStatement("SELECT job_id, title, job_description FROM jobs WHERE title ILIKE ? AND workstyle = ? AND job_type = ? AND department = ?");
+            statement.setString(1, "%" + q + "%");
+            statement.setString(2, workstyle);
+            statement.setString(3, jobtype);
+            statement.setString(4, industry);
+            ResultSet res = statement.executeQuery();
+            List<Map<String, Object>> list = new ArrayList<>();
+            while (res.next()) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("job_id", res.getInt("job_id"));
+                map.put("title", res.getString("title"));
+                map.put("job_description", res.getString("job_description"));
+                list.add(map);
+            }
+            res.close();
+            statement.close();
+            connection.close();
+            return list;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
